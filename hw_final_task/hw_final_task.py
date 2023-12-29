8f2bbf83783c59210e550f0fbc764e32cbff6981
Dmitry <dimatimoha2014@gmail.com>
2023-12-29 05:03:23 +0300

    solving without using get_dummies was added

--- hw_final_task/hw_final_task.py
+++ hw_final_task/hw_final_task.py
@@ -15,3 +15,14 @@ pd.get_dummies(data['whoAmI'])
 
 
 # Ссылка на решение данной задачи в colab (последняя строка с кодом) https://colab.research.google.com/drive/1NzekPM2afVqmeoEXKzVsN21yIvQ9_Hp4
+
+
+#Решение без get_dummies
+lst1 = list(data['whoAmI'])
+lst_human = []
+lst_robot = []
+for i in data['whoAmI']:
+    lst_human.append(i=='human')
+    lst_robot.append(i=='robot')
+hot_data = pd.DataFrame({'human':lst_human,'robot':lst_robot})
+print(hot_data)
